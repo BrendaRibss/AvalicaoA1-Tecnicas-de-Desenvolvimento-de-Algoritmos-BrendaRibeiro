dc67c47cb8c1afe04b057a1a26fa363fcad80932
BrendaRibss <brenndha.ribs@hotmail.com>
2025-11-26 20:46:54 -0300

    Add files via upload

---
+++ EstruturasdeRepeticao/UsandoFor.py
@@ -0,0 +1,9 @@
+# Exemplo usando FOR. 
+ 
+#O For cria um Loop que percorre os números de 1 até 100.
+for num in range(1, 101):
+
+    # Verifica se o número é par (resto da divisão por 2 é zero)
+    if num % 2 == 0:
+        
+        print(num)
