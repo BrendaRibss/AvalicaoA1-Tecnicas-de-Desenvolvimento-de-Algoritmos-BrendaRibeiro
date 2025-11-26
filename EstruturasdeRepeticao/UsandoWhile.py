42d2b602336305818a6a582bcf2018ff52248bfa
BrendaRibss <brenndha.ribs@hotmail.com>
2025-11-26 20:58:47 -0300

    Enhance UsandoWhile.py with explanatory comments
    Added comments to clarify the while loop functionality.

--- EstruturasdeRepeticao/UsandoWhile.py
+++ EstruturasdeRepeticao/UsandoWhile.py
@@ -1,5 +1,15 @@
+# Exemplo usando While
+
+# A variável de contagem é inicializada com o valor 1
 num = 1
+
+# Enquanto o número for menor ou igual a 100, o loop continua
 while num <= 100:
+
+    # Verifica se o número atual é par, é par caso o resto da divisão seja 0
     if num % 2 == 0:
+  
         print(num)
+
+    # Incrementa o valor da variável num em 1 para avançar a contagem
     num += 1
